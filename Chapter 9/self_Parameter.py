a6e511e370a7eb47f9639374d33e1b2ea9ed9582
mannaf <mannap2@gmail.com>
2019-12-18 00:35:16 +0600

    Use the words mysillyobject and abc instead of self

---
+++ Chapter 9/self_Parameter.py
@@ -0,0 +1,11 @@
+class Persion:
+    def __init__(mysillyobject, name, age):
+        mysillyobject.name = name
+        mysillyobject.age = age
+
+    def myfun(abc):
+        print("hello This is " + abc.name)
+
+
+p1 = Persion("Mannaf", 25)
+p1.myfun()
